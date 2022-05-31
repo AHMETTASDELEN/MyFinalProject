9b3eee0975cde0a8cc65c200694d4b6d09760cc8
AHMETTASDELEN <ahmettasdelen5499@gmail.com>
2022-05-31 20:18:54 +0300

    31.05.2022

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -16,7 +16,7 @@ namespace ConsoleUI
         private static void CategoryTest()
         {
             CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
-            foreach (var category in categoryManager.GetAll())
+            foreach (var category in categoryManager.GetAll().Data)
             {
                 Console.WriteLine(category.CategoryName);
             }
@@ -24,33 +24,36 @@ namespace ConsoleUI
 
         private static void ProductTest()
         {
-            ProductManager productManager = new ProductManager(new EfProductDal());
+            ProductManager productManager = new ProductManager
+                (new EfProductDal(),
+                new CategoryManager(new EfCategoryDal()));
 
-            //var result = productManager.GetAll();
-            //if (result.Success == true)
-            //{
-            //    foreach (var product in result.Data)
-            //    {
-            //        Console.WriteLine(product.ProductName);
-            //    }
-            //}
-            //else
-            //{
-            //    Console.WriteLine(result.Message);
-            //}
-
-            var result = productManager.GetProductDetails();
-            if (result.Success==true)
+            var result = productManager.GetAll();
+            if (result.Success == true)
             {
                 foreach (var product in result.Data)
                 {
-                    Console.WriteLine(product.ProductName + " / " + product.CategoryName);
+                    Console.WriteLine(product.ProductName);
+                    Console.WriteLine(result.Message);
                 }
             }
             else
             {
                 Console.WriteLine(result.Message);
             }
+
+            //var result = productManager.GetProductDetails();
+            //if (result.Success==true)
+            //{
+            //    foreach (var product in result.Data)
+            //    {
+            //        Console.WriteLine(product.ProductName + " / " + product.CategoryName);
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine(result.Message);
+            //}
         }
     }
 }
